e1d4b9c49ca3e3437c28c426a6627f313b01f6e3
abrahan <f8abrahan@gmail.com>
2019-05-11 21:07:16 -0500

    Termino app blog

--- blog/views.py
+++ blog/views.py
@@ -11,4 +11,6 @@ def blog(request):
 def category(request, category_id):
     # category = Category.objects.get(id=category_id)
     category = get_object_or_404(Category, id=category_id)
+    # posts = Post.objects.filter(categories=category)
+    # return render(request,"blog/category.html", {'category':category, 'posts':posts})
     return render(request,"blog/category.html", {'category':category})
